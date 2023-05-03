dea2c2b6095ec7822050c35b1ecd19183262fd93
TudorModVis <131814733+TudorModVis@users.noreply.github.com>
2023-05-03 12:38:33 +0300

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "pages/*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
