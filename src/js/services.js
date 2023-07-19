5c0978a4080b7bef23793d2500f72d6f780bd97b
TudorModVis <131814733+TudorModVis@users.noreply.github.com>
2023-07-19 20:05:24 +0300

    Add files via upload

--- src/js/services.js
+++ src/js/services.js
@@ -3,7 +3,14 @@ const wrapper = service.querySelector('.wrapper');
 
 function moveWrapper() {
     let prog = service.getBoundingClientRect().top * -1 / service.clientHeight;
+
+    // let text = document.querySelector('#test').getBoundingClientRect().bottom;
+    // if (text == window.innerHeight) {
+    //     console.log(prog);
+    // }
+
     wrapper.style.transform = 'translateY(' + prog * 100 + '%)';
+
 }
 
 window.addEventListener('scroll', moveWrapper);
