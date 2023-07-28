722fdd911dd947998f0a99ef5459872e2976b1dc
TudorModVis <131814733+TudorModVis@users.noreply.github.com>
2023-07-28 11:16:50 +0300

    Add files via upload

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,7 +2,6 @@ const imageObserver = new IntersectionObserver (entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('active');
-            entry.target.querySelector('video').play();
         }
     })
 })
@@ -10,3 +9,11 @@ const imageObserver = new IntersectionObserver (entries => {
 document.querySelectorAll('.image').forEach(image => {
     imageObserver.observe(image);
 });
+
+
+document.body.addEventListener('touchstart', () => {
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+        if (!video.playing) video.play();
+    });
+})
